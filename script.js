4f512dd0f8444aeb58413c6ad28e295a92edf219
Leah Loeser <leahloeser@newschool.edu>
2023-12-05 20:51:13 -0500

    enter key triggers API request

--- script.js
+++ script.js
@@ -1,19 +1,34 @@
-const baseURL = "https://modern-birthstones.onrender.com" // add render URL
+// render URL
+const baseURL = "https://modern-birthstones.onrender.com" 
 
-const inputMonth = "May"; // user input here
+ // user input
+const inputMonth = "";
+
+// add event listener to the input field to listen for "Enter" key press
+document.getElementById("inputMonth").addEventListener("keyup", function(event) {
+    if (event.key === "Enter") {
+        // if "Enter" key is pressed, trigger the getStones function
+        getStones();
+    }
+});
 
 const getStones = async function() {
     try {
+        // get user input
         const inputMonth = document.getElementById("inputMonth").value;
+        // log user input
         console.log("Input month:", inputMonth);
 
+        // request info from API
         const stoneIDs = await fetch(`${baseURL}?month=${encodeURIComponent(inputMonth)}`);
         const stoneIDsJSON = await stoneIDs.json();
+        // get something back
         console.log("Stone IDs:", stoneIDsJSON);
 
+        // define output field
         const stoneList = document.getElementById("stoneList");
 
-        // Clear previous results
+        // clear previous output results
         stoneList.innerHTML = "";
 
         for (const key of stoneIDsJSON) {
@@ -22,6 +37,7 @@ const getStones = async function() {
                 const stoneInfoJSON = await stoneInfo.json();
                 console.log("Stone Info:", stoneInfoJSON);
 
+                // update html
                 const listItem = document.createElement("li");
                 const stoneText = document.createTextNode(`Month: ${inputMonth}, Birthstone: ${stoneInfoJSON.stone}`);
 
