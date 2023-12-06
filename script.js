24dbb1422146e25d350b4ec5ce8bed6d5ec1bf90
Leah Loeser <leahloeser@newschool.edu>
2023-12-05 19:27:54 -0500

    idk

--- script.js
+++ script.js
@@ -2,26 +2,26 @@ const baseURL = "https://modern-birthstones.onrender.com" // add render URL
 
 const monthPick = "May"; // user input here
 
-
 const getStones = async function(){
-    try { // request info from API
-        const stoneIDs = await fetch(`${baseURL}?month=${encodeURIComponent(monthPick)}`);
+    try {
+        // info from user
+        const inputMonth = document.getElementById("inputMonth").value;
+        // request info from API
+        const stoneIDs = await fetch(`${baseURL}?month=${encodeURIComponent(inputMonth)}`);
         const stoneIDsJSON = await stoneIDs.json();
 
         const stoneList = document.getElementById("stoneList");
-        // get something back
-        //console.log(stoneIDsJSON)
 
-        Object.keys(stoneIDsJSON).forEach(async (key, value) => {
+        // clear previous results
+        stoneList.innerHTML = "";
+
+        for (const key of stoneIDsJSON) {
             try {
-                const stoneInfo = await fetch(`${baseURL}/stone/${stoneIDsJSON[key]}`);
+                const stoneInfo = await fetch(`${baseURL}/stone/${key}`);
                 const stoneInfoJSON = await stoneInfo.json();
 
-                console.log(stoneInfoJSON)
-
-                // update html
                 const listItem = document.createElement("li");
-                const stoneText = document.createTextNode(`Month: ${monthPick}, Birthstone: ${stoneInfoJSON.stone}`);
+                const stoneText = document.createTextNode(`Month: ${inputMonth}, Birthstone: ${stoneInfoJSON.stone}`);
 
                 listItem.appendChild(stoneText);
                 stoneList.appendChild(listItem);
@@ -29,7 +29,7 @@ const getStones = async function(){
             } catch (error) {
                 console.error("Error fetching stone info:", error);
             }
-        });
+        }
 
     } catch (error) {
         console.log(error);
