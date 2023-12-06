aa284f0b42970bda9c48114db3d3251933257328
Leah Loeser <leahloeser@newschool.edu>
2023-12-06 16:48:24 -0500

    image path updates

--- index.js
+++ index.js
@@ -8,18 +8,18 @@ app.use(cors());
 const port = process.env.PORT || 5502;
 
 let birthstones = {
-    1: { month: "January", stone: "Garnet", imageUrl: "./images/january.png" },
-    2: { month: "February", stone: "Amethyst", imageUrl: "./images/february.png" },
-    3: { month: "March", stone: "Aquamarine & Bloodstone", imageUrl: "./images/march.png" },
-    4: { month: "April", stone: "Diamond", imageUrl: "./images/april.png" },
-    5: { month: "May", stone: "Emerald", imageUrl: "./images/may.png" },
-    6: { month: "June", stone: "Alexandrite, Pearl, & Moonstone", imageUrl: "./images/june.png" },
-    7: { month: "July", stone: "Ruby", imageUrl: "./images/july.png" },
-    8: { month: "August", stone: "Peridot", imageUrl: "./images/august.png" },
-    9: { month: "September", stone: "Sapphire", imageUrl: "./images/september.png" },
-    10: { month: "October", stone: "Tourmaline & Opal", imageUrl: "./images/october.png" },
-    11: { month: "November", stone: "Citrine", imageUrl: "./images/november.png" },
-    12: { month: "December", stone: "Tanzanite, Turquoise, & Zircon", imageUrl: "./images/december.png" },
+    1: { month: "January", stone: "Garnet", imageUrl: "/images/january.png" },
+    2: { month: "February", stone: "Amethyst", imageUrl: "/images/february.png" },
+    3: { month: "March", stone: "Aquamarine & Bloodstone", imageUrl: "/images/march.png" },
+    4: { month: "April", stone: "Diamond", imageUrl: "/images/april.png" },
+    5: { month: "May", stone: "Emerald", imageUrl: "/images/may.png" },
+    6: { month: "June", stone: "Alexandrite, Pearl, & Moonstone", imageUrl: "/images/june.png" },
+    7: { month: "July", stone: "Ruby", imageUrl: "/images/july.png" },
+    8: { month: "August", stone: "Peridot", imageUrl: "/images/august.png" },
+    9: { month: "September", stone: "Sapphire", imageUrl: "/images/september.png" },
+    10: { month: "October", stone: "Tourmaline & Opal", imageUrl: "/images/october.png" },
+    11: { month: "November", stone: "Citrine", imageUrl: "/images/november.png" },
+    12: { month: "December", stone: "Tanzanite, Turquoise, & Zircon", imageUrl: "/images/december.png" },
 };
 
 
